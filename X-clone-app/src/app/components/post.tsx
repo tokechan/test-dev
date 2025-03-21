aa65d78d99a48b210c917fbf3afa9e19ba18c53f
tokechan <fleatoke@gmail.com>
2025-03-21 22:40:46 +0900

    fix:shadcn

--- X-clone-app/src/app/components/post.tsx
+++ X-clone-app/src/app/components/post.tsx
@@ -3,6 +3,7 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { useState } from "react"
 import { client } from "@/lib/client"
+import { Button } from "@/components/ui/button"
 
 export const RecentPost = () => {
   const [name, setName] = useState<string>("")
@@ -62,13 +63,13 @@ export const RecentPost = () => {
           onChange={(e) => setName(e.target.value)}
           className="w-full text-base/6 rounded-md bg-black/50 hover:bg-black/75 focus-visible:outline-none ring-2 ring-transparent  hover:ring-zinc-800 focus:ring-zinc-800 focus:bg-black/75 transition h-12 px-4 py-2 text-zinc-100"
         />
-        <button
+        <Button
           disabled={isPending}
           type="submit"
           className="rounded-md text-base/6 ring-2 ring-offset-2 ring-offset-black focus-visible:outline-none focus-visible:ring-zinc-100 ring-transparent hover:ring-zinc-100 h-12 px-10 py-3 bg-brand-700 text-zinc-800 font-medium bg-gradient-to-tl from-zinc-300 to-zinc-200 transition hover:bg-brand-800"
         >
           {isPending ? "Creating..." : "Create Post"}
-        </button>
+        </Button>
       </form>
     </div>
   )
