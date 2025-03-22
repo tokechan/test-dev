bc0af09f337afd870de6a06f7b6e48a76c693afe
tokechan <fleatoke@gmail.com>
2025-03-23 02:12:01 +0900

    Add:PostFrom PostList SideMenu

---
+++ X-clone-app/src/components/SideMenu.tsx
@@ -0,0 +1,13 @@
+import { SignOutButton } from "@clerk/nextjs";
+import React from "react";
+
+function SideMenu() {
+    return (
+        <div>
+            <SignOutButton />
+        </div>
+    );
+}
+
+export default SideMenu;
+
