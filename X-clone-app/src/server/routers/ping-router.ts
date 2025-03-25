5e5d6a357afe2ebd61eec2adfee178ecff68f424
tokechan <fleatoke@gmail.com>
2025-03-25 22:08:22 +0900

    Add:cloudinary media storage

--- X-clone-app/src/server/routers/ping-router.ts
+++ X-clone-app/src/server/routers/ping-router.ts
@@ -1,8 +1,10 @@
 import { posts, users } from "@/server/db/schema";
 import { desc, eq } from "drizzle-orm";
 import { j, publicProcedure } from "../jstack";
+import { z } from "zod";
+import { uploadImage } from "@/lib/cloudenary";
 
-export const postRouter = j.router({
+export const pingRouter = j.router({
   all: publicProcedure.query(async ({ c, ctx }) => {
     const { db } = ctx;
 
@@ -22,4 +24,40 @@ export const postRouter = j.router({
       .orderBy(desc(posts.createdAt));
     return c.superjson(postsData);
   }),
+
+  create: publicProcedure
+    .input(
+        z.object({
+            content: z.string().min(1),
+            handle: z.string().min(1),
+            image: z.string().optional(),
+        })
+    )
+    .mutation(async ({ c, ctx, input }) => {
+        const { db } = ctx;
+        const { content, handle, image } = input;
+
+        let imageUrl = null;
+        if (image) {
+            try {
+                imageUrl = await uploadImage(image);
+            } catch (error) {
+                console.error("Error uploading image:", error);
+            }
+            const post = await db.insert(posts).values({
+                content,
+                handle,
+                image: imageUrl,
+            });
+
+            return c.superjson({post});
+        }
+
+        const post = await db.insert(posts).values({
+            content,
+            handle,
+        });
+
+        return c.superjson({post});
+    }),
 });
--- X-clone-app/src/server/routers/post-router.ts
+++ X-clone-app/src/server/routers/post-router.ts
@@ -1,6 +1,8 @@
 import { posts, users } from "@/server/db/schema";
 import { desc, eq } from "drizzle-orm";
 import { j, publicProcedure } from "../jstack";
+import { z } from "zod";
+import { uploadImage } from "@/lib/cloudenary";
 
 export const postRouter = j.router({
   all: publicProcedure.query(async ({ c, ctx }) => {
@@ -22,4 +24,34 @@ export const postRouter = j.router({
       .orderBy(desc(posts.createdAt));
     return c.superjson(postsData);
   }),
+
+  create: publicProcedure
+    .input(
+        z.object({
+            content: z.string().min(1),
+            handle: z.string(),
+            image: z.string().optional(),
+        })
+    )
+    .mutation(async ({ c, ctx, input }) => {
+        const { db } = ctx;
+        const { content, handle, image } = input;
+
+        let imageUrl = null;
+        if (image) {
+            try {
+                imageUrl = await uploadImage(image);
+            } catch (error) {
+                console.error("Error uploading image:", error);
+            }
+        }
+
+        const post = await db.insert(posts).values({
+            content,
+            handle,
+            image: imageUrl,
+        });
+
+        return c.superjson({post});
+    }),
 });
